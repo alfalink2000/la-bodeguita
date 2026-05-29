74187cdd15b03a0d372c0c2f79d1080cfaf47e64
Leonel Machin Remis <alfalink2000@gmail.com>
2026-05-29 18:33:57 +0200

    cambios backend 10

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,15 @@ import { store } from "./store/store";
 import App from "./App.jsx";
 import "./index.css";
 
-// // ✅ REGISTRO SIMPLIFICADO DEL SERVICE WORKER
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/sw.js")
-//       .then((reg) => console.log("✅ SW registrado:", reg))
-//       .catch((err) => console.log("❌ SW error:", err));
-//   });
-// }
+// ✅ REGISTRO SIMPLIFICADO DEL SERVICE WORKER
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("✅ SW registrado:", reg))
+      .catch((err) => console.log("❌ SW error:", err));
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
